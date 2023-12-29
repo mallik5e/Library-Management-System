fed762552e89b96a64736761bf35bb867c63add1
MALLIKARJUNASWAMY H M <manumallu81@gmail.com>
2023-12-30 02:42:11 +0530

    Library Management System

---
+++ Library_Management_System/src/jframe/DefaultPieDataset.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jframe;
+
+/**
+ *
+ * @author manum
+ */
+class DefaultPieDataset {
+
+    void setValue(String iPhone_5s, Double aDouble) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ Library_Management_System/src/jframe/PiePlot.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jframe;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author manum
+ */
+class PiePlot {
+
+    void setSectionPaint(String iPhone_5s, Color color) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setBackgroundPaint(Color white) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
